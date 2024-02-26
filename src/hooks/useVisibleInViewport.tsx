b65f8b0f9f753ca80203d634df685f534f4fa5b8
Matt Bremmers <bremmdev@gmail.com>
2024-02-26 18:46:10 +0100

    add options to useVisibleInViewport

--- src/hooks/useVisibleInViewport.tsx
+++ src/hooks/useVisibleInViewport.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 
 /**
+ * Options for the useVisibleInViewport hook.
+ * @typedef {Object} Options
+ * @property {boolean} [once=true] - If true, the callback will be triggered only once; otherwise, it will be triggered every time the element becomes visible.
+ * @property {number} [threshold=0] - A number between 0 and 1 indicating the percentage of the element's visibility the observer's callback should be executed at.
+ */
+
+/**
  * Custom hook that triggers a callback when a specified element becomes visible in the viewport.
  *
  * @template T The HTML element type.
  * @param {React.RefObject<T>} elementRef The ref of the element to observe.
  * @param {() => void} cb The callback function to execute when the element becomes visible.
- * @param {boolean} [once=true] If true, the callback will be triggered only once; otherwise, it will be triggered every time the element becomes visible.
+ * @param {Options} [options={}] The options for the hook.
  * @returns {void}
  */
+
+type Options = {
+  once?: boolean;
+  threshold?: number;
+};
 
 export function useVisibleInViewport<T extends HTMLElement>(
   elementRef: React.RefObject<T>,
   cb: () => void,
-  once = true
+  { once = true, threshold = 0 }: Options = {}
 ) {
   React.useEffect(() => {
     function handleIntersect(
@@ -31,7 +43,11 @@ export function useVisibleInViewport<T extends HTMLElement>(
       }
     }
 
-    const observer = new IntersectionObserver(handleIntersect);
+    const observer = new IntersectionObserver(handleIntersect, {
+      root: null,
+      rootMargin: "0px",
+      threshold,
+    });
     const targetElement = elementRef.current;
     if (targetElement) {
       observer.observe(targetElement);
@@ -42,5 +58,5 @@ export function useVisibleInViewport<T extends HTMLElement>(
         observer.unobserve(targetElement);
       }
     };
-  }, [cb, once, elementRef]);
+  }, [cb, once, threshold, elementRef]);
 }
